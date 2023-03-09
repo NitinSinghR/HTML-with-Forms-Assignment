ca5ea0c19d38461a5d7b25aa1a1ff51cfff97c43
NitinSinghR <nitin.singh@tringapps.com>
2023-03-09 18:43:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 function validateForm() {
-    var date = document.forms["myForm"]["date"].value;
-    var email = document.forms["myForm"]["email"].value;
-    var mobile = document.forms["myForm"]["mobile"].value;
+    let date = document.forms["myForm"]["date"].value;
+    let mobile = document.forms["myForm"]["mobile"].value;
 
-    var emailRegex = /^\S+@\S+\.\S+$/;
-    var mobileRegex = /^[1-9]\d{9}$/;
-    var dateRegex = /^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}$/;
+    let mobileRegex = /^[1-9]\d{9}$/;
+    let dateRegex = /^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}$/;
 
     if (!dateRegex.test(date)) {
         alert("Please enter a valid date in DD-MM-YYYY format.");
-        return false;
-    }
-    if (!emailRegex.test(email)) {
-        alert("Please enter a valid email address");
         return false;
     }
     if (!mobileRegex.test(mobile)) {
